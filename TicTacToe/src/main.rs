4af46ec49ab566ddbcbbb84fea3a0ca861b0672a
Sebastian Yde Madsen <madsen3008@gmail.com>
2023-04-07 17:34:02 +0200

    Updated tictactoe

--- TicTacToe/src/main.rs
+++ TicTacToe/src/main.rs
@@ -3,25 +3,225 @@ type GridContentType = char;
 // Constant variables
 const N_ROWS: u32 = 3;
 const N_COLS: u32 = 3;
+const N_ROUNDS: u32 = 6;
+const NAN_TOKEN: char = 'N';
+const __INDEX_MAP__: [[i32; 2]; 9]  = [[0,0], [0,1], [0,2],
+                                       [1,0], [1,1], [1,2],
+                                       [2,0], [2,1], [2,2]];
 
 
 fn print_grid(grid: &[[GridContentType; N_COLS as usize]; N_ROWS as usize]) {
-    println!("===== PRINTING GRID =====");
+    println!("\nCurrent Board:");
     for row in grid.iter() {
-            println!("     {:?} ", row);
+        println!("-------------");
+        print!("| ");
+        for entry in row.iter() {
+            if entry != &NAN_TOKEN
+            {
+                print!("{} | ", entry);
+            }
+            else
+            {
+                print!("  | ");
+            }
+        }
+        println!(" ");
     }
-    println!("========================");
+    println!("-------------");
+}
+
+fn print_description() {
+    println!(" ");
+    println!(" ################## TicTacToe Game Description ###################");
+    println!(" #                                                               #");
+    println!(" #    1.) Entries in the board are indexed with integers, as:    #");
+    println!(" #                       -------------                           #");
+    println!(" #                       | 1 | 2 | 3 |                           #");
+    println!(" #                       -------------                           #");
+    println!(" #                       | 4 | 5 | 6 |                           #");
+    println!(" #                       -------------                           #");
+    println!(" #                       | 7 | 8 | 9 |                           #");
+    println!(" #                       -------------                           #");
+    println!(" #                                                               #");
+    println!(" #    2.) The 2 players will take turns to place their piece     #");
+    println!(" #        on the board.                                          #");
+    println!(" #                                                               #");
+    println!(" #    3.) Each of the 2 players will specify the placement of    #");
+    println!(" #        their piece by an integer: 1,2,...,9, according to     #");
+    println!(" #        the formatting displayed in 1.) above.                 #");
+    println!(" #                                                               #");
+    println!(" #################################################################");
+}
+
+fn has_winner(grid: &[[GridContentType; N_COLS as usize]; N_ROWS as usize]) -> (bool, bool) {
+    // Checking rows
+    for row in 0..3 {
+        if grid[row][0] == grid[row][1] && grid[row][1] == grid[row][2] {
+            if grid[row][0] == 'X'{
+                return (true, false);
+            }
+            else if grid[row][0] == 'O' {
+                return (false, true)
+            }
+        }
+    }
+    // Checking columns
+    for col in 0..3 {
+        if grid[0][col] == grid[1][col] && grid[1][col] == grid[2][col] {
+            if grid[0][col] == 'X'{
+                return (true, false);
+            }
+            else if grid[0][col] == 'O' {
+                return (false, true)
+            }
+        }
+    }
+    // Checking diagonals
+    if grid[0][0] == grid[1][1] && grid[1][1] == grid[2][2] {
+        if grid[0][0] == 'X'{
+            return (true, false);
+        }
+        else if grid[0][0] == 'O' {
+            return (false, true)
+        }
+    }
+    if grid[2][0] == grid[1][1] && grid[1][1] == grid[2][0] {
+        if grid[2][0] == 'X'{
+            return (true, false);
+        }
+        else if grid[2][0] == 'O' {
+            return (false, true)
+        }
+    }
+    return (false, false);
+}
+
+fn is_over(grid: &[[GridContentType; N_COLS as usize]; N_ROWS as usize]) -> bool {
+    let (mut x_counter, mut o_counter): (u8, u8) = (0, 0);
+    for row in grid.iter() {
+        for entry in row.iter() {
+            if *entry == 'X'{
+                x_counter += 1;
+            }
+            else if *entry == 'O' {
+                o_counter += 1;
+            }
+
+            if x_counter == 3 || o_counter == 3 {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+fn set_piece(grid: &mut[[GridContentType; N_COLS as usize]; N_ROWS as usize],
+             input_index: &i32,
+             piece_char: &char)
+{
+    let coordinates = __INDEX_MAP__[(*input_index-1) as usize];
+    grid[coordinates[0] as usize][coordinates[1] as usize] = *piece_char;
+}
+
+fn is_occupied(grid: &mut[[GridContentType; N_COLS as usize]; N_ROWS as usize],
+               input_index: &i32) -> bool
+{
+    let coordinates = __INDEX_MAP__[(*input_index-1) as usize];
+    if grid[coordinates[0] as usize][coordinates[1] as usize] != NAN_TOKEN {
+        return true;
+    }
+    return false
 }
 
 fn main() {
+
+    print_description();
+
     // Fixed-size array.
-    let mut grid: [[GridContentType; N_COLS as usize]; N_ROWS as usize] = [['-'; N_COLS as usize]; N_ROWS as usize];
+    let mut grid: [[GridContentType; N_COLS as usize]; N_ROWS as usize] = [[NAN_TOKEN; N_COLS as usize]; N_ROWS as usize];
 
-    // Fill the grid with values.
-    grid[0][1] = 'X';
-    grid[1][1] = 'X';
-    grid[1][2] = 'O';
+    // Starting game loop
+    for _round in 0..N_ROUNDS {
 
-    print_grid(&grid);
+        // Player 1 input
+        println!("Player 1 ('X') - type a number and press Enter: ");
+        let mut player_1_index_str = String::new();
+        std::io::stdin().read_line(&mut player_1_index_str).expect("Player 1 - Failed to read line.");
+        let mut player_1_index = player_1_index_str.trim().parse::<i32>().expect("Player 1 - Invalid input");
+
+        // Checking that number is withing range
+        if player_1_index < 1 || player_1_index > 9 {
+            println!("Should be a number in [1;9] - please enter a new number!");
+            while player_1_index < 1 || player_1_index > 9 {
+                std::io::stdin().read_line(&mut player_1_index_str).expect("Player 1 - Failed to read line.");
+                player_1_index = player_1_index_str.trim().parse::<i32>().expect("Player 1 - Invalid input");
+            }
+        }
+        // Checking the index is unoccupied
+        if is_occupied(&mut grid, &player_1_index) {
+            println!("Index: {} is occupied! - please enter a new number", player_1_index);
+            while is_occupied(&mut grid, &player_1_index) {
+                std::io::stdin().read_line(&mut player_1_index_str).expect("Player 1 - Failed to read line.");
+                player_1_index = player_1_index_str.trim().parse::<i32>().expect("Player 1 - Invalid input");
+            }
+        }
+        set_piece(&mut grid, &player_1_index, &'X');
+        print_grid(&grid);
+
+        // Checking for game finished
+        if has_winner(&grid) == (true, false) {
+            println!("\n # PLAYER 1 HAS WON - GAME FINISHED! ");
+            break;
+        }
+        else if has_winner(&grid) == (false, true) {
+            println!("\n # PLAYER 2 HAS WON - GAME FINISHED! ");
+            break;
+        }
+        else if is_over(&grid) {
+            println!("\n # TIEBREAK - NO WINNER! ");
+            break;
+        }
+
+        // Player 2 input
+        println!("Player 2 ('O') - type a number and press Enter: ");
+        let mut player_2_index_str = String::new();
+        std::io::stdin().read_line(&mut player_2_index_str).expect("Player 2 - Failed to read line.");
+        let mut player_2_index = player_2_index_str.trim().parse::<i32>().expect("Player 2 - Invalid input");
+
+        // Checking that number is withing range
+        if player_2_index < 1 || player_2_index > 9 {
+            println!("Should be a number in [1;9] - please enter a new number!");
+            while player_2_index < 1 || player_2_index > 9 {
+                std::io::stdin().read_line(&mut player_2_index_str).expect("Player 2 - Failed to read line.");
+                player_2_index = player_2_index_str.trim().parse::<i32>().expect("Player 2 - Invalid input");
+            }
+        }
+        // Checking the index is unoccupied
+        if is_occupied(&mut grid, &player_2_index) {
+            println!("Index: {} is occupied! - please enter a new number", player_2_index);
+            while is_occupied(&mut grid, &player_2_index) {
+                std::io::stdin().read_line(&mut player_2_index_str).expect("Player 2 - Failed to read line.");
+                player_2_index = player_2_index_str.trim().parse::<i32>().expect("Player 2 - Invalid input");
+            }
+        }
+
+        set_piece(&mut grid, &player_2_index, &'O');
+        print_grid(&grid);
+
+        // Checking for game finished
+        if has_winner(&grid) == (true, false) {
+            println!("\n # PLAYER 1 HAS WON - GAME FINISHED! ");
+            break;
+        }
+        else if has_winner(&grid) == (false, true) {
+            println!("\n # PLAYER 2 HAS WON - GAME FINISHED! ");
+            break;
+        }
+        else if is_over(&grid) {
+            println!("\n # TIEBREAK - NO WINNER! ");
+            break;
+        }
+    }
+
 
 }
