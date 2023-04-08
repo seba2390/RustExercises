33bf72eaeefc8b36d2090cff7325d2a7b48d53d1
Sebastian Yde Madsen <madsen3008@gmail.com>
2023-04-08 22:26:57 +0200

    update

--- MyMatrixLib/tests/unit_tests.rs
+++ MyMatrixLib/tests/unit_tests.rs
@@ -3,8 +3,10 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 #[cfg(test)]
-mod tests {
+mod initializers {
     use MyMatrixLib::Matrix;
+    const MAX_SIZE: usize = 300;
+    const MIN_SIZE: usize = 1;
 
     fn test_matrix_ones_creation_<T>()
         where
@@ -22,11 +24,14 @@ mod tests {
             Clone +
             Copy
     {
-        let ones_mat = Matrix::<T>::ones(2,2);
-        assert_eq!(ones_mat[(0, 0)], T::one());
-        assert_eq!(ones_mat[(0, 1)], T::one());
-        assert_eq!(ones_mat[(1, 0)], T::one());
-        assert_eq!(ones_mat[(1, 1)], T::one());
+        for size in MIN_SIZE..MAX_SIZE {
+            let ones_mat = Matrix::<T>::ones(size,size);
+            for row_idx in 0..size {
+                for col_idx in 0..size {
+                    assert_eq!(ones_mat[(row_idx, col_idx)], T::one());
+                }
+            }
+        }
     }
 
     #[test]
@@ -59,11 +64,14 @@ mod tests {
             Clone +
             Copy
     {
-        let ones_mat = Matrix::<T>::zeros(2,2);
-        assert_eq!(ones_mat[(0, 0)], T::zero());
-        assert_eq!(ones_mat[(0, 1)], T::zero());
-        assert_eq!(ones_mat[(1, 0)], T::zero());
-        assert_eq!(ones_mat[(1, 1)], T::zero());
+        for size in MIN_SIZE..MAX_SIZE {
+            let zeros_mat = Matrix::<T>::zeros(size,size);
+            for row_idx in 0..size {
+                for col_idx in 0..size {
+                    assert_eq!(zeros_mat[(row_idx, col_idx)], T::zero());
+                }
+            }
+        }
     }
 
     #[test]
@@ -96,11 +104,19 @@ mod tests {
             Clone +
             Copy
     {
-        let ones_mat = Matrix::<T>::identity(2);
-        assert_eq!(ones_mat[(0, 0)], T::one());
-        assert_eq!(ones_mat[(0, 1)], T::zero());
-        assert_eq!(ones_mat[(1, 0)], T::zero());
-        assert_eq!(ones_mat[(1, 1)], T::one());
+        for size in MIN_SIZE..MAX_SIZE {
+            let identity_mat = Matrix::<T>::identity(size);
+            for row_idx in 0..size {
+                for col_idx in 0..size {
+                    if col_idx == row_idx {
+                        assert_eq!(identity_mat[(row_idx, col_idx)], T::one());
+                    }
+                    else {
+                        assert_eq!(identity_mat[(row_idx, col_idx)], T::zero());
+                    }
+                }
+            }
+        }
     }
 
     #[test]
